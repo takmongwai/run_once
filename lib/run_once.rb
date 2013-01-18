f74792cde4fe23c59c9c56de5c7009f3899c36dd
dewang wei <dewang.wei@gmail.com>
2013-01-18 16:18:21 +0800

    fix read old pid nil to integer error

--- lib/run_once.rb
+++ lib/run_once.rb
@@ -11,9 +11,9 @@ module RunOnce
     FileUtils.mkdir_p(pid_dir) unless File.exists?(pid_dir)
     pid_file = File.join( pid_dir,%Q~run_once_#{args[:file_name].gsub(/\/|\./,'')}.pid~ )
     old_pid = File.open(pid_file).read.to_s.to_i if File.exists?(pid_file)
+    old_pid ||= 0
 
     begin
-      raise Errno::ESRCH if old_pid <= 0
       Process.getpgid(old_pid)
       return yield(old_pid) if block_given?
       return old_pid
--- lib/run_once/version.rb
+++ lib/run_once/version.rb
@@ -1,3 +1,3 @@
 module RunOnce
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
